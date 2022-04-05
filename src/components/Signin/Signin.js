b0faebd5b345f808ac67d6c3ecd40276e8e80f09
Tasha <ncarr63@gmail.com>
2022-04-05 15:32:38 +1200

    chnage signin

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     }
  
     onSubmitSignIn = () => {
-        fetch('https://sheltered-river-39504.herokuapp.com/signin', {
+        fetch('https://sheltered-river-39504.herokuapp.com', {
             method: 'post',
             mode: "cors",
             headers: {
