0fe44435e45e322a560e659f064bd064db7873e9
Tasha <ncarr63@gmail.com>
2022-04-05 12:12:33 +1200

    changed signin

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     }
  
     onSubmitSignIn = () => {
-        fetch('https://sheltered-river-39504.herokuapp.com/signin', {
+        fetch('localhost:3000/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json' },
             body: JSON.stringify({
